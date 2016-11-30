08548276dcf35b8055550cc1b5c967e7622b28c7
gitsgr <patilsagar4488@gmail.com>
2016-11-30 16:32:19 +0530

    Second activity added

--- app/src/main/java/code/rahul/myapplication/MainActivity.java
+++ app/src/main/java/code/rahul/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package code.rahul.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void go(View view) {
+        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(intent);
     }
 }
